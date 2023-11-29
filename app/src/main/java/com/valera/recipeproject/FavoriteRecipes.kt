858733228d2330d2a4c6068f63bb82af98f90fe6
reece-vlr <reece.vlr@gmail.com>
2023-11-29 12:26:56 +0800

    Add viewFavoriteRecipes function

--- app/src/main/java/com/valera/recipeproject/FavoriteRecipes.kt
+++ app/src/main/java/com/valera/recipeproject/FavoriteRecipes.kt
@@ -3,6 +3,7 @@ package com.valera.recipeproject
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class FavoriteRecipes : AppCompatActivity() {
@@ -21,5 +22,35 @@ class FavoriteRecipes : AppCompatActivity() {
     private fun startMainActivity() {
         val i = Intent(this, MainActivity :: class.java)
         startActivity(i)
+    }
+
+    private fun viewFavoriteRecipes() {
+        val databaseHandler = DatabaseHandler(this)
+
+        val recipe: List<RecipeModelClass> = databaseHandler.viewRecipe()
+        val recipeArrayName = Array(recipe.size) {"null"}
+        val recipeArrayFavorite = Array(recipe.size) {false}
+
+        var index = 0
+
+        for (r in recipe) {
+            // Verify if recipe is favorite
+            if (!r.favorite) {
+                index++
+            }
+            else {
+                recipeArrayName[index] = r.name
+                recipeArrayFavorite[index] = r.favorite
+
+                index++
+            }
+        }
+
+        val listView =
+            findViewById<ListView>(R.id.listView)
+        val listAdapter =
+            ListAdapter(this, recipeArrayName, recipeArrayFavorite)
+
+        listView.adapter = listAdapter
     }
 }
