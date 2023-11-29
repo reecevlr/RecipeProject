f6b082c227d996632bb8bc084ae558d2d99ab3b3
reece-vlr <reece.vlr@gmail.com>
2023-11-29 15:09:06 +0800

    Add viewFavoriteRecipes function (no real-time updates)

--- app/src/main/java/com/valera/recipeproject/FavoriteRecipes.kt
+++ app/src/main/java/com/valera/recipeproject/FavoriteRecipes.kt
@@ -7,9 +7,14 @@ import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class FavoriteRecipes : AppCompatActivity() {
+
+    private lateinit var listAdapter: ListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.favorite_recipes)
+
+        viewFavoriteRecipes()
 
         val btnReturn = findViewById<Button>(R.id.btnReturn)
 
@@ -22,5 +27,35 @@ class FavoriteRecipes : AppCompatActivity() {
     private fun startMainActivity() {
         val i = Intent(this, MainActivity :: class.java)
         startActivity(i)
+    }
+
+    private fun viewFavoriteRecipes() {
+        val databaseHandler = DatabaseHandler(this)
+
+        val allRecipes: List<RecipeModelClass> = databaseHandler.viewRecipe()
+
+        // Filter recipes by favorite only
+        val favoriteRecipes = allRecipes.filter { it.favorite }
+
+        val recipeArrayId = favoriteRecipes.map { it.id ?: 0}.toTypedArray()
+        val recipeArrayName = favoriteRecipes.map { it.name }.toTypedArray()
+        val recipeArrayFavorite = favoriteRecipes.map { it.favorite }.toTypedArray()
+
+        val listView = findViewById<ListView>(R.id.listView)
+
+        listAdapter = ListAdapter(this, recipeArrayName, recipeArrayFavorite) { position ->
+            // Handles favorite click
+            val recipeId = recipeArrayId[position]
+            val newFavoriteStatus = !recipeArrayFavorite[position]
+
+            // Updates favorite status in database
+            databaseHandler.updateFavoriteStatus(recipeId, newFavoriteStatus)
+
+            // Reflect changes to array
+            recipeArrayFavorite[position] = newFavoriteStatus
+            listAdapter.notifyDataSetChanged()
+        }
+
+        listView.adapter = listAdapter
     }
 }
